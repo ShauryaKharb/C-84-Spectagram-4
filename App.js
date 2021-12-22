b13c60416fea2b1320bb3f949e69587bb443783a
ShauryaKharb <73641410+ShauryaKharb@users.noreply.github.com>
2021-12-23 00:38:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+import DrawerNavigator from './navigation/DrawerNavigation'
+import { NavigationContainer } from '@react-navigation/native'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <DrawerNavigator />
+      </NavigationContainer>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
